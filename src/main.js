ce8ac1539a09cdf8a1c1113a34ef61ec18f754f1
Alexei Andreanov <aalexei@ibs.re.kr>
2022-01-31 17:36:58 +0900

    src/main.js: 	1. use pen.js 	2. commented out the function start

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-function start(tab) {
-    chrome.windows.create({
-        url: chrome.runtime.getURL("test.html"),
-        type: "popup",
-        focused: true
-    });
+//function start(tab) {
+//    chrome.windows.create({
+//        url: chrome.runtime.getURL("test.html"),
+//        type: "popup",
+//        focused: true
+//    });
 //    let url = chrome.runtime.getURL("test.html");
 //    let tab = await chrome.tabs.create({url});
-}
+//}
 
 //chrome.browserAction.onClicked.addListener(start);
-chrome.runtime.onInstalled.addListener(
 //chrome.runtime.onClicked.addListener(start);
+chrome.runtime.onInstalled.addListener(
     async () => {
         chrome.windows.create({
-            url: chrome.runtime.getURL("test.html"),
+            url: chrome.runtime.getURL("pen.html"),
 //            type: "popup",
             focused: true
         });
